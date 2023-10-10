28bf3cc1ddb54b6c46be4b8cac3a1f77c3819e98
Joao Rocha <joao.rocha@wobcom.de>
2023-10-10 08:43:03 +0200

    modified installAudio to work for both drivers, added everything Cmd

--- cmd/update.go
+++ cmd/update.go
@@ -1,8 +1,9 @@
 package cmd
 
 import (
-	"os"
 	"fmt"
+	"os"
+	"os/exec"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -17,41 +18,103 @@ func init() {
 		Args: cobra.NoArgs,
 	}
 
-	var audio  = &cobra.Command {
+	var audio = &cobra.Command {
 		Use: "audio",
 		Short: "Install audio drivers",
 		Long: "After kernel uppdates audio drivers need to be reinstalled [requires sudo]",
 		Args: cobra.NoArgs,
 		Run: func(cmd *cobra.Command, args []string) {
-			installAudio()
+			installDriver(
+				"audio/",
+				"./install.cirrus.driver.sh",
+				viper.GetString("audioGitRepo"),
+			)
 			if reboot {
-				
+				rebootCmd()
 			}
 		},
 	}
+
+	var bluetooth = &cobra.Command {
+		Use: "bluetooth",
+		Short: "Install bluetooth drivers",
+		Long: "After kernel uppdates bluetooth drivers need to be reinstalled [requires sudo]",
+		Args: cobra.NoArgs,
+		Run: func(cmd *cobra.Command, args []string) {
+			installDriver(
+				"bluetooth/",
+				"./install.bluetooth.sh",
+				viper.GetString("bluetoothGitRepo"),
+			)
+			if reboot {
+				rebootCmd()
+			}
+		},
+	}
+
+	var everything = &cobra.Command {
+		Use: "everything",
+		Short: "Install all drivers",
+		Long: "After kernel uppdates all drivers need to be reinstalled [requires sudo]",
+		Args: cobra.NoArgs,
+		Run: func(cmd *cobra.Command, args []string) {
+			installDriver(
+				"audio/",
+				"./install.cirrus.driver.sh",
+				viper.GetString("audioGitRepo"),
+			)
+			installDriver(
+				"bluetooth/",
+				"./install.bluetooth.sh",
+				viper.GetString("bluetoothGitRepo"),
+			)
+			if reboot {
+				rebootCmd()
+			}
+		},
+	} 
+
 	audio.Flags().BoolVarP(&reboot, "reboot", "r", viper.GetBool("restart"), "reboot system after updates")
+	bluetooth.Flags().BoolVarP(&reboot, "reboot", "r", viper.GetBool("restart"), "reboot system after updates")
+	everything.Flags().BoolVarP(&reboot, "reboot", "r", viper.GetBool("restart"), "reboot system after updates")
 	rootCmd.AddCommand(update)
+	update.AddCommand(audio, bluetooth, everything)
 }
 
-func installAudio() {
-	audioFolder := "audio/"
-	install_cmd_audio := "./install.cirrus.driver.sh"
-	ExecuteCommand(
-		[]string{"git", "clone", viper.GetString("audio_git_repo"), audioFolder},
+func handleError(err error) {
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func rebootCmd() {
+	reboot := exec.Command("sudo", "reboot")
+	err := reboot.Run()
+	if err != nil {
+		fmt.Println("error: could not reboot")
+	}
+}
+
+func installDriver(folder string, cmd string, repo string) {
+	err := ExecuteCommand(
+		[]string{"git", "clone", repo, folder},
 		"downloading needed repository...",
 		"error: there was a problem downloading the needed files",
 	)
-	os.Chdir(audioFolder)
-	ExecuteCommand(
-		[]string{install_cmd_audio},
+	handleError(err)
+	os.Chdir(folder)
+	err = ExecuteCommand(
+		[]string{cmd},
 		"installing the audio drivers...",
 		"error: there was a problem installing the drivers",
 	)
+	handleError(err)
 	os.Chdir("../")
-	ExecuteCommand(
-		[]string{"rm", "-rf", audioFolder},
+	err = ExecuteCommand(
+		[]string{"rm", "-rf", folder},
 		"removing repository...",
 		"error: there was a problem removing the repository",
 	)
+	handleError(err)
 	fmt.Println("installed audio drivers successfuly")
 }
